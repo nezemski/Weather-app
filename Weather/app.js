a4f57277174679666e9ffb8fadd375736ff74866
adiscord <adiscord@yandex.ru>
2024-07-22 12:05:14 +0300

    1

---
+++ Weather/app.js
@@ -0,0 +1,50 @@
+const apiKey = "063a36c724828d312a4635af0e1be8a1";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
+
+const searchInput = document.querySelector(".search-box input");
+const searchButton = document.querySelector(".search-box button");
+const weatherIcon = document.querySelector(".image-weather i");
+const weather = document.querySelector(".weather");
+const error = document.querySelector(".error");
+
+async function searchWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  if (response.status === 400) {
+    error.style.display = "block";
+    weather.style.display = "none";
+  } else {
+    const data = await response.json();
+
+    console.log(data, "data");
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "&#8451";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind-speed").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clear") {
+      weatherIcon.className = "fa-solid fa-sun";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.className = "fa-solid fa-cloud-rain";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.className = "fa-solid fa-cloud-mist";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.className = "fa-solid fa-cloud-drizzle";
+    }
+
+    error.style.display = "none";
+    weather.style.display = "block";
+  }
+}
+
+searchButton.addEventListener("click", () => {
+  searchWeather(searchInput.value);
+  searchInput.value = "";
+});
+
+searchInput.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) {
+    searchWeather(searchInput.value);
+    searchInput.value = "";
+  }
+});
